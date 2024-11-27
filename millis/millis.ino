1ff23fa927c337ac7a3965bec560aebc08b32695
Gabriel <Gabrieldb050890@gmail.com>
2024-11-27 13:55:01 +0100

    Ejercicio terminado

---
+++ millis/millis.ino
@@ -0,0 +1,22 @@
+/*  millis
+a. Repetir el ejercicio 2 usando la función millis().
+*/
+
+uint64_t ultimaImpresion = 0;
+int cont = 0;
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+// Creamos un contador pero usamos temporizaciones mediante consulta de ciclos millis()  micros()
+  
+  uint64_t momentoActual = millis();
+  
+  if((momentoActual - ultimaImpresion)>1000){
+      Serial.print("Numero : ");
+      Serial.println(cont);
+      cont++;
+      ultimaImpresion =momentoActual;
+}
+}
