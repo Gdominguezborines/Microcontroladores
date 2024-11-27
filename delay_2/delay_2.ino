21f2ef2842f4ace836d06eb3e684023ea14deb10
Gabriel <Gabrieldb050890@gmail.com>
2024-11-27 13:53:11 +0100

    Ejercicio terminado

---
+++ delay_2/delay_2.ino
@@ -0,0 +1,42 @@
+/*
+a. El contador ctr1 se incrementará 10 veces por segundo 
+b. El contador ctr2 se incrementará 1 vez cada 2 segundos. 
+c. Cuando se incremente ctr2 se descontará el valor de dicho contador de ctr1. 
+d. Cada vez que uno de los dos contadores se incremente se imprimirá con el formato
+   C1: {valor del contador 1} C2: {valor del contador 2}. e. Usar la función delay.
+*/
+
+int ctr1 = 0;
+int ctr2 = 0;
+int seg = 0;
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  //Saca el contador 10 veces e 1 seg por pantalla
+  for(int i=0; i<10; i++){
+    ctr1++;
+    imprimir();
+  }
+
+  if(seg%2==0 ){
+    //Aumenta el 2º contador cada dos seg, resta al 1º contador el valor del 2º y lo saca por pantalla
+    ctr2++;
+    ctr1-ctr2;
+    imprimir();
+  }
+//variable de segundos 
+  seg++;
+  delay(1000);
+}
+
+void imprimir(){
+  //funcion para mostrar los datos 
+Serial.print("ctr1 : ");
+Serial.println(ctr1);
+Serial.print("ctr2 : ");
+Serial.println(ctr2);
+
+
+}
