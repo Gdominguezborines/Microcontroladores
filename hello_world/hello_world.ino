4601396671cce705abfd1eeae0de71b4dbbc442e
Gabriel <Gabrieldb050890@gmail.com>
2024-11-27 10:09:50 +0100

    1 resuelto

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+  Serial.println("START");
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("hello_world");
+
+}
